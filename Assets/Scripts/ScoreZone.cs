c2b6b97efd3b5b888a4ddb1a5c206e2c78b30034
Bruno Briante <geekpobre@gmail.com>
2014-07-20 19:24:03 -0300

    adding score

---
+++ Assets/Scripts/ScoreZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this make the unity editor create a box collider2d when you drag the script to a gameobject
+[RequireComponent(typeof(BoxCollider2D))]
+public class ScoreZone : MonoBehaviour {
+	// now you can drag the scoreboard from the editor instead of using a tag or a name
+	public Scoreboard scoreboard;
+	
+	void Start() {
+		// you shoud probably do it on the editor instead of here but just to be safe
+		collider2D.isTrigger = true;
+	}
+
+	// this is called when something enter the collider
+	void OnTriggerEnter2D(Collider2D go) {
+		// this check if the colliding object is the ball
+		Debug.Log(go.gameObject.name == "Ball");
+		if (go.gameObject.name == "Ball") {
+			// this destroy the ball after 0.3 second. the time is just to make the ball move a bit after hitting the paddle
+			Destroy(go.gameObject, 0.3f);
+			// this call the function that add the point to player
+			scoreboard.ScorePoint(1);
+		}
+	}
+}
---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUIText))]
+public class Scoreboard : MonoBehaviour {
+
+	private int score = 0;
+	public GameObject ball;
+
+	public void ScorePoint(int points) {
+		// add points to actual score
+		score += points;
+
+		// set the score variable to the gui text
+		guiText.text = score.ToString();
+
+		// instantiate a new ball prefab
+		GameObject newBall = Instantiate(ball, Vector3.zero, Quaternion.identity) as GameObject;
+
+		// bit hacky. the scorezone check for the gameobject name and the new ball is called Ball (Clone) instead of just Ball
+		// this change the name of the new instace of the ball gameobject
+		newBall.name = "Ball";
+
+	}
+}
